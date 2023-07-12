83f9f2a55db2be45fa332c7dfcb33948a69d4138
Daniel Galuardi <dan.galuardi@gmail.com>
2023-07-12 15:02:07 -0400

    Cart Page Logic

--- src/app/pages/cart/cart.component.ts
+++ src/app/pages/cart/cart.component.ts
@@ -24,14 +24,33 @@ export class CartComponent implements OnInit {
     'action'
   ]
 
-  constructor(private CartService: CartService) { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
-    this.dataSource = this.cart.items;
+    this.cartService.cart.subscribe((_cart: Cart) => {
+      this.cart = _cart;
+      this.dataSource = this.cart.items;
+    })
   }
 
   getTotal(items: Array<CartItem>): number {
-    return this.CartService.getTotal(items);
+    return this.cartService.getTotal(items);
+  }
+
+  onClearCart(): void {
+    this.cartService.clearCart();
+  }
+
+  onRemoveFromCart(item: CartItem): void {
+    this.cartService.removeFromCart(item);
+  }
+
+  onAddQuantity(item: CartItem): void {
+    this.cartService.addToCart(item);
+  }
+
+  onRemoveQuantity(item: CartItem): void {
+    this.cartService.removeQuantity(item);
   }
 
 }
